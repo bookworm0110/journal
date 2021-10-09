b0399535b2f1c98c8a303166178d12add13915cc
Ian Koh <plasma@Ians-Air.hitronhub.home>
2021-10-08 17:30:29 -0700

    oct 8(2)

--- postgresdb.py
+++ postgresdb.py
@@ -1,7 +1,7 @@
 import psycopg2
-DB_URL = 'postgres://postgres:Go761204@localhost:5432/postgres'
+# DB_URL = 'postgres://postgres:Go761204@localhost:5432/postgres'
 
-
+DB_URL = 'postgres://uguqaccpkekgpf:2aa052969770a6158d1c58873f0ec9f8853901e3a1ce6947d24cfb4ce55e838a@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d5m6oa5s29nsp5'
 def pgconn():
     conn = None
     try:
