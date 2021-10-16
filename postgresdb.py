4a5841e03fe9f2c956fc80f1f6c67502ee101e97
Ian Koh <plasma@Ians-Air.hitronhub.home>
2021-10-15 17:08:12 -0700

    commit for run

--- postgresdb.py
+++ postgresdb.py
@@ -5,7 +5,7 @@ DB_URL = 'postgres://uguqaccpkekgpf:2aa052969770a6158d1c58873f0ec9f8853901e3a1ce
 def pgconn():
     conn = None
     try:
-        conn = psycopg2.connect(DB_URL)
+        conn = psycopg2.connect(DB_URL,sslmode='require')
     except Exception as e:
         print(e)
     return conn
