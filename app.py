c459b627258324d93c84faed66e1dcf2fe8a0597
Ian Koh <iankoh@Ians-Air.hitronhub.home>
2022-02-07 17:19:06 -0800

    a git commit

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def admin():
     for row in rows:
         # jnl = Journals(row['id'], row['author'], row['emotion'],
         #    row['weather'], row['content'], row['date'])
-        jnl = [Journals(row[0], row[1], row[2], row[3], row[4], row[5], row[6]),row[7]]
+        jnl = Journals(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
         jnllist.append(jnl)
     return render_template("adminpanel.html", jnllist=jnllist, user=current_user)
 
